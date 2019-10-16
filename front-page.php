ce722b983855e1f1552f2d01d340684430d9fe90
阿部寛子 <hihoyeahi@gmail.com>
2019-10-16 22:39:06 +0900

    firstcommit

---
+++ front-page.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="">
+	<link rel="stylesheet" type="text/css" href="assets/style.css">
+
+	<title>南三陸アウトドアフェスタ</title>
+</head>
+<body>
+	
+<header id="header">
+	<div class="global-nabi-containr">
+		<nav>
+			<ul>
+				<li>ホーム</li>
+				<li>ブログ一覧</li>
+				<li>お問い合わせ</li>
+			</ul>
+		</nav>
+	</div>
+</header>
+<main>
+	<section id="main">
+		<div class="header">
+			<h1>神割崎感謝祭 みなみさんりくアウトドアフェスタ</h1>
+			<div>
+				お天気のコンテンツ
+			</div>	
+		</div>
+		<!-- メインのスライダーを入れる -->
+		<div class="top_containts">
+			<div class="main-sride">
+				<div class="srider_filter">
+					jsが来る
+				</div>	
+				<ul id="srider_index">
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</div>	
+			<div class="main-index">
+			</div>
+		</div>
+	
+		 <!-- こんなことやります　プログラムの案内 -->
+		 <div id="program" class="">
+		 	<h2>こんなことやります </h2>
+		 	<li><img src="" alt="" ></li>
+		 	<li><img src="" alt="" ></li>
+		 	<li><img src="" alt="" ></li>
+		 	<li><img src="" alt="" ></li>
+		 </div>
+
+		<div class="infoomation_container">
+			<div id="infomation">
+				ここにループを置く
+			</div>
+		</div>
+	</section>
+
+
+
+</main>
+<footer>
+	
+</footer>	
+
+
+
+</body>
+</html>
