7691d1a41f36851f2381fe7bf7cce49187f8a694
Hiroko ABE <hihoyeahi@gmail.com>
2019-12-26 17:42:16 +0900

    -frontpage_php_reedit -奥行きをなくす -以前のサイトで使用していたファイルを別納 -オリジナルのMAPデータからCSVデータを作成 -背景の削除 -translate3dの削除

--- front-page.php
+++ front-page.php
@@ -12,52 +12,30 @@
 <body>
 	<main>
 		<section>
-			<div class="cover_img"></div>
 			<div class="video_wrappar">
 			
 				<div class="map_container">
 						<div  type="image/svg+xml" style="z-index: 1" class="map map_base">
 							<img class="base_img" src="<?php echo get_template_directory_uri(); ?>/img/map_base.svg" usemap="#tgitem" alt="戸倉ネイチャーパークトップページに表示する地図のベース画像" />
 						</div>
-						<div  type="image/svg+xml" style="z-index: 2" class="map main_title">
-							<img src="<?php echo get_template_directory_uri(); ?>/img/map_title.svg">	
+						<div  type="image/svg+xml" style="z-index: 2" class="map map eagle shake">
+							<img class=""src="<?php echo get_template_directory_uri(); ?>/img/map_eagle.svg" alt="戸倉ネイチャーパークトップページに表示しているサイトオオワシのイラスト">	
 						</div>
-						<div  type="image/svg+xml" style="z-index: 3" class="map sub_title">
-							<img class=""src="<?php echo get_template_directory_uri(); ?>/img/map_sub_title.svg" alt="戸倉ネイチャーパークトップページに表示しているサイトのサブタイトルの画像">	
+						<div  type="image/svg+xml" style="z-index: 3" class="map">
+							<img class="horizontal" src="<?php echo get_template_directory_uri(); ?>/img/map_description.svg" alt="戸倉ネイチャーパークトップページに表示している体験スポットの説明">
 						</div>
-						
-						<div  type="image/svg+xml" style="z-index: 4" class="map index ">
-							<img class="" src="<?php echo get_template_directory_uri(); ?>/img/map_index.svg" alt="戸倉ネイチャーパークトップページに表示している体験スポットの名称">
-						</div>
-						<div  type="image/svg+xml" style="z-index: 5" class="map label vertical">
+						<div  type="image/svg+xml" style="z-index: 4" class="map label">
 							<img class="horizontal" src="<?php echo get_template_directory_uri(); ?>/img/map_label.svg" alt="戸倉ネイチャーパークトップページに表示している体験スポットの説明">
 						</div>
-	
-						<div  type="image/svg+xml" style="z-index: 6" class="map label_sec vertical_sec">
-							<img class="holizontal_sec"src="<?php echo get_template_directory_uri(); ?>/img/map_lavel_2.svg" alt="戸倉ネイチャーパークトップページに表示している体験スポットの名称">
+						<div  type="image/svg+xml" style="z-index: 5"  class="map item">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/map_item.svg" usemap="#tgitem" alt="戸倉ネイチャーパークトップページに表示している体験スポットのイラスト">
 						</div>
-						<div  type="image/svg+xml" style="z-index: 17"  class="map item">
-							<img src="<?php echo get_template_directory_uri(); ?>/img/map_overitem.svg" usemap="#tgitem" alt="戸倉ネイチャーパークトップページに表示している体験スポットのイラスト">
-						</div>
-						<div  type="image/svg+xml" style="z-index: 18" class="map item icon">
-							<img  class="item_dwn"src="<?php echo get_template_directory_uri(); ?>/img/map_overitem_dwn.svg" usemap="#tgitem" alt="戸倉ネイチャーパークトップページに表示している体験スポットのイラスト">
-						</div>
-						<div  type="image/svg+xml" style="z-index: 19" class="map item icon">
-							<img class="item_up"src="<?php echo get_template_directory_uri(); ?>/img/map_overitem_up.svg" usemap="#tgitem" alt="戸倉ネイチャーパークトップページに表示している体験スポットのイラスト">
-						</div>
-						<div  type="image/svg+xml" style="z-index: 20"class="map item icon " >
-							<img class="item_up_2"src="<?php echo get_template_directory_uri(); ?>/img/map_overitem_up_2.svg" usemap="#tgitem" alt="戸倉ネイチャーパークトップページに表示している体験スポットのイラスト">
-						</div>
-						
-						<div  type="image/svg+xml" style="z-index: 11" class="map keybox" id="">
+						<div  type="image/svg+xml" style="z-index: 6" class="map keybox" id="">
 							<img class="" src="<?php echo get_template_directory_uri(); ?>/img/map_keybox.svg" usemap="#tgitem" alt="" />
 							<img class="guide flash" src="<?php echo get_template_directory_uri(); ?>/img/guide_2.svg" alt="戸倉ネイチャーパークトップページに表示している同じサーバー上で公開しているサイトへ遷移する宝箱の画像">
-						</div>	
-						<div  type="image/svg+xml" style="z-index: 12" class="map eagle shake">
-							<img  src="<?php echo get_template_directory_uri(); ?>/img/map_eagle.svg"　alt="戸倉ネイチャーパークトップページに表示しているオオワシの画像">
 						</div>
-						<div  type="image/svg+xml" style="z-index: 13" class="map compass">
-							<img class="" src="<?php echo get_template_directory_uri(); ?>/img/map_compass.svg"　alt="戸倉ネイチャーパークトップページに表示している方位磁石の画像">
+						<div  type="image/svg+xml" style="z-index: 7" class="map compass" id="">
+							<img class="" src="<?php echo get_template_directory_uri(); ?>/img/map_compass.svg" usemap="" alt="" />
 						</div>
 					</div>
 				</div>
@@ -68,9 +46,9 @@
 					  <area onFocus="this.blur();" shape="rect" coords="91,108,121,134" href="https://o-eels.org/" alt="おきなくらイールズホームページへ遷移" target="_blank" />
 					  <area onFocus="this.blur();" shape="rect" coords="143,90,179,100" href="#" alt="漁業体験" target="__blank" />
 					  <area onFocus="this.blur();" shape="rect" coords="142,66,167,79" href="https://m-inuwashi.jp/" alt="南三陸町ネイチャーセンターホームページへ遷移" target="_blank" />
-					  <area onFocus="this.blur();" shape="rect" coords="124,38,148,55" href="http://studio-elephant.com/flow/" alt="波伝の森山学校ホームページへ遷移" target="_blank" />
+					  <area onFocus="this.blur();" shape="rect" coords="124,38,148,55" href="https://www.facebook.com/yamagakko/" alt="波伝の森山学校ホームページへ遷移" target="_blank" />
 					  <area onFocus="this.blur();" shape="rect" coords="105,71,134,81" href="http://kawatouminovisitorcenter.jp/umi/index.html" alt="海のビジターセンターホームページへ遷移" target="_blank" />
-					  <area onFocus="this.blur();" shape="rect" coords="24,67,52,82" href="神割キャンプ場ホームページへ遷移" alt="神割キャンプ場ホームページへ遷移" target="_blank" />
+					  <area onFocus="this.blur();" shape="rect" coords="24,67,52,82" href="https://www.m-kankou.jp/kamiwari-camp/" alt="神割キャンプ場ホームページへ遷移" target="_blank" />
 					  <area onFocus="this.blur();" shape="rect" coords="87,59,115,71" href="https://www.pref.miyagi.jp/soshiki/sz-seinen/" alt="志津川自然の家ホームページへ遷移" target="_blank" />
 					  <area onFocus="this.blur();" shape="rect" coords="3,104,52,148" href="<?php echo get_permalink( HOME );?> "alt="" target="_blank"/>
 					</map>
@@ -78,7 +56,6 @@
 			
 
 			</div>
-		</div>
 	</section>
 
 	</main>
